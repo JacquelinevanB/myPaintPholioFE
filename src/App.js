1456ef5059e2c9d79aa0be6591f5bbc4f48f5b06
JacquelinevanB <jacqueline.van.burk@gmail.com>
2022-08-20 22:50:16 +0200

    navigation & hero

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, NavLink } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
 import LandingPage from "./pages/landingPage/LandingPage";
 import DashboardAdmin from "./pages/dashboard-admin/DashboardAdmin";
@@ -11,40 +11,40 @@ import ProjectUpdatePage from "./pages/projectUpdatepage/ProjectUpdatePage";
 function App() {
     return (
         <>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/dashboard_admin">
-                            Admin
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/user">
-                            Dashboard
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/user/project">
-                            Project pagina
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/user/all_projects">
-                            Projecten overzicht
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/user/project/update">
-                            Projectupdate pagina
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+            {/*<nav>*/}
+            {/*    <ul>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/">*/}
+            {/*                Home*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/dashboard_admin">*/}
+            {/*                Admin*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/user">*/}
+            {/*                Dashboard*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/user/project">*/}
+            {/*                Project pagina*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/user/all_projects">*/}
+            {/*                Projecten overzicht*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*        <li>*/}
+            {/*            <NavLink to="/user/project/update">*/}
+            {/*                Projectupdate pagina*/}
+            {/*            </NavLink>*/}
+            {/*        </li>*/}
+            {/*    </ul>*/}
+            {/*</nav>*/}
             <Switch>
                 <Route exact path="/">
                     <LandingPage />
--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import './LandingPage.css';
+import Navigation from "../../components/navigation/Navigation";
+import Hero from "../../components/hero/Hero";
+import Button from "../../components/button/Button";
+import DescriptionTile from "../../components/descriptiontile/DescriptionTile";
+import painthand from "../../assets/painting-hand.jpg";
+import painttubes from "../../assets/paint-tubes.jpg";
+
 
 function LandingPage(props) {
     return (
         <>
-            <h1>Landingpage</h1>
+            <header id="header" className="outer-content-container">
+                <div className="inner-content-container">
+                    <Navigation id="navigation"/>
+                    <Hero id="hero"/>
+                </div>
+            </header>
+
+            <main id="main" className="outer-content-container">
+                <div className="inner-content-container">
+                    <section className="description">
+                        <DescriptionTile img={painthand}
+                                         imgDescription={"easel with painting hand"}
+                                         title={"Cum distinctio"}>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor id magnam maxime
+                                molestias odio quidem, quis tempora unde vel vero. Architecto debitis dignissimos dolor
+                                eaque esse est ex excepturi, facere harum ipsum laboriosam quam quas quis rem sit,
+                                temporibus veniam.</p>
+                        </DescriptionTile>
+                        <DescriptionTile img={painttubes}
+                                         imgDescription={"tubes with oil paint"}
+                                         title={"Suspendisse potenti"}>
+                            <p>Donec sed sagittis augue. Vestibulum ante ipsum primis in faucibus orci luctus et
+                                ultrices posuere cubilia curae; Aenean a vestibulum dolor, a ultrices neque. Donec
+                                vehicula tortor erat, in sagittis orci viverra eu. Suspendisse accumsan pretium neque,
+                                quis volutpat est eleifend nec. Cras consectetur lobortis laoreet. Vestibulum in turpis
+                                urna. Aliquam erat volutpat. Nulla ex enim, bibendum in velit ut, rutrum gravida
+                                felis.</p>
+                        </DescriptionTile>
+                    </section>
+                    {/*<section className="">*/}
+
+                    {/*</section>*/}
+                    {/*<section className="">*/}
+
+                    {/*</section>*/}
+                </div>
+            </main>
+
+            <footer>
+
+            </footer>
+
         </>
     );
 }
